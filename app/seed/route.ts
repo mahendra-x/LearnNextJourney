a27ca758b566b87eec82e93dda7039a289526a4a
mahendra-x <ymahendra1995s@gmail.com>
2025-01-27 17:33:27 +0530

    rebase

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,6 +102,7 @@
 // }
 
 export async function GET() {
+  
   return Response.json({
     message:
       'Uncomment this file and remove this line. You can delete this file when you are finished.',
